59ee247defd7029f3abde54e7f578a5ced9145ad
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2023-11-18 21:27:10 +0100

    done wt insertion sort

--- sort.h
+++ sort.h
@@ -16,10 +16,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void swap(int *a, int *b);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-
 #endif /*_SORT_H_ */
