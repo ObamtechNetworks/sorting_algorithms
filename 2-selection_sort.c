d803b8ed26127edd77096d4ee1085495e2034595
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-19 15:58:07 +0200

    Fixed indentation

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,25 +9,26 @@
 */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min;
-    int tmp;
+	size_t i, j, min;
+	int tmp;
 
 	if (!array || !size)
 		return;
-    for (i = 0; i < size - 1; i++)
-    {
-        min = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min])
-                min = j;
-        }
-        if (min != i)
-        {
-            tmp = array[i];
-            array[i] = array[min];
-            array[min] = tmp;
-            print_array(array, size);
-        }
-    }
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
+	}
 }
