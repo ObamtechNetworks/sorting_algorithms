d3fe71cdeadeb3ba9f4a678644e68b392a7bf873
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-20 20:52:32 +0200

    Updating sort.h

--- sort.h
+++ sort.h
@@ -18,7 +18,6 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void swap(int *a, int *b);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
